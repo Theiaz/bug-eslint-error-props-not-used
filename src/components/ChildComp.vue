33c8f5b07e2c3d15a8bc932fbe835dc9b0596921
Julian Schäfer <schaefer@synyx.de>
2022-07-20 13:44:32 +0200

    Initial commit

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,12 @@
+<script setup>
+// "props' is assigned a value but never used"
+const props = defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <h2>{{ msg }}</h2>
+  <!-- This would work -->
+  <!-- <h2>{{ props.msg  }}</h2> -->
+</template>
